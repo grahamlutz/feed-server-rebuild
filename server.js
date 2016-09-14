7f86b39aacccf8b604d3c5ad01fabea59c533768
Graham Lutz <graham@brainbytescreative.com>
2016-09-14 16:47:41 -0400

    updates names

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var logger = require('morgan');
-var bodyparse = require('bodyparser');
+var bodyparse = require('body-parser');
 var mongoose = require('mongoose');
 
 var app = express();
--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var expect = chai.expect;
 var should = chai.should();
 var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
-var server = require('./server');
+var server = require('../server');
 var Product = mongoose.model('Product');
 
 chai.use(chaiHttp);
