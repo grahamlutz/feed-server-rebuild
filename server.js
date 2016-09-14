7304b45c152db4d32a2464c5f572636d3e8f648b
Graham Lutz <graham@brainbytescreative.com>
2016-09-14 16:32:09 -0400

    add basic server

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var fs = require('fs');
+var logger = require('morgan');
+var bodyparse = require('bodyparser');
+var mongoose = require('mongoose');
+
+var app = express();
