6aafb8266d79f8917d0445818defaf1561e3a76e
Graham Lutz <graham@brainbytescreative.com>
2016-09-14 16:32:54 -0400

    adds basic test stubs

---
+++ test/test.js
@@ -0,0 +1,16 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var chaiHttp = require('chai-http');
+var mongoose = require('mongoose');
+var server = require('./server');
+var Product = mongoose.model('Product');
+
+chai.use(chaiHttp);
+
+describe('Products', function() {
+
+
+});
