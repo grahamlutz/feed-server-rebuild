959ff3e44f3dc0088869c8f82e5b31266cbba5c3
Graham Lutz <graham@brainbytescreative.com>
2016-09-14 16:56:17 -0400

    fixes typo

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var expect = chai.expect;
 var should = chai.should();
 var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
-var server = require('../mserver');
+var server = require('../server');
 var Product = mongoose.model('Products');
 
 chai.use(chaiHttp);
