fb373a8913b6a137e12530d5ed14136523556c6f
Graham Lutz <graham@brainbytescreative.com>
2016-09-14 22:18:50 -0400

    saves products to DB - still giving error

--- routes/index.js
+++ routes/index.js
@@ -4,38 +4,58 @@ var fs = require('fs');
 var path = require('path');
 var router = express.Router();
 var mongoose = require('mongoose');
+var Product = mongoose.model('Products');
 
-/* GET home page. */
+/* GET /refresh - refresh the db with product data. */
 router.get('/refresh', function(req, res, next) {
   var bodyContent = '';
 
   http.get('http://storage1.merchantadvantage.com/macm2543/AllRegionProducts.txt').on('response', function (response) {
+
     response.on('data', function (chunk) {
         bodyContent += chunk;
     });
+
     response.on('end', function (data) {
+
         var lines = bodyContent.split('\n');
+        //console.log('lines: ', lines);
         var result = [];
-        var headers=lines[0].split("\t");
+        var headers = lines[0].split("\t");
+        //console.log('headers: ', headers);
 
         for( var i=1; i<lines.length; i++ ){
 
       	  var obj = {};
-      	  var currentline=lines[i].split("\t");
+      	  var currentline = lines[i].split("\t");
 
       	  for( var j=0; j<headers.length; j++){
       		  obj[headers[j]] = currentline[j];
       	  }
 
       	  result.push(obj);
+
+          var product = new Product(result[i-1]);
+
+          product.save(function(err, product) {
+            if (err) return next(err);
+          });
         }
-        console.log(result[0])
-        res.write("hello");
-        res.end();
+
+        res.end("Success! " + result.length + " products updated.");
     }).on('error', (e) => {
       console.log(`Got error: ${e.message}`);
     })
   });
 });
+
+/* GET all products */
+router.get('/products', function(req, res, next) {
+  console.log('router.get /products');
+   Product.find(function(err, gear) {
+     if (err) return next(err);
+     res.json(gear);
+   })
+ });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,16 +10,29 @@ var app = express();
  *  Database connection
  */
 
-require('./models/Products');
+require('./models/products');
 //mongoose.Promise = global.Promise;
 var dbConfig = require('./config/db-connection')
 mongoose.connect(dbConfig.mongoURI[app.settings.env], function(err, res) {
   if(err) {
     console.log('Error connecting to the database. ' + err);
   } else {
+    mongoose.connection.db.dropDatabase();
     console.log('Connected to Database: ' + dbConfig.mongoURI[app.settings.env]);
   }
 });
+
+/*
+ * Middleware
+ */
+
+ app.use(express.static(__dirname + '../public'));
+ app.engine('html', require('ejs').renderFile);
+ app.set('view engine', 'html');
+
+/*
+ *  Port Setup
+ */
 
 app.set('port', 8080);
 app.listen(app.get('port'));
@@ -29,7 +42,6 @@ app.listen(app.get('port'));
  */
 
 var routes = require('./routes');
-
 app.use('/', routes);
 
 /*
